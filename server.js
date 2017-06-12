fe075028434e24b6de73e1a4b5cd7c65cfd9b5b1
Rishi <dgreat.rishi@gmail.com>
2017-06-12 11:34:45 +0400

    corrected path and port
    corrected path and port

--- server.js
+++ server.js
@@ -2,21 +2,22 @@ var express = require('express');
 var app = express();
 var path    = require("path");
 
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static('/'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use('/styles',  express.static(__dirname + '/app/styles'));
 app.use('/app',  express.static(__dirname + '/app'));
 app.use('/views',  express.static(__dirname + '/app/views'));
-app.use('/favicon.ico',  express.static(__dirname + '/bower_components'));
+app.use('/favicon.ico',  express.static(__dirname + '/favicon.ico'));
 
 app.get('/', function (req, res) {
   console.log('Agent App is running');
-  res.sendFile(path.join(__dirname+'/favicon.ico'));
+  res.sendFile(path.join(__dirname+'/index.html'));
 }).listen(app.get('port'), function() {
     console.log('App is running, server is listening on port ', app.get('port'));
 });
 
-app.set('port', (process.env.PORT || 5000));
+
 
 //For avoidong Heroku $PORT error
 /*app.get('/', function(request, response) {
