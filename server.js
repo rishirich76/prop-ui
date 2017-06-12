1a0a1ef2f8e2b6cfc1956380c7203f248de57f66
Rishi <dgreat.rishi@gmail.com>
2017-06-12 10:59:40 +0400

    port changed
    port changed

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(5000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
