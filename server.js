1a0721c18f205f790794463d391f0fb592a3574a
Rishi <dgreat.rishi@gmail.com>
2017-06-12 10:33:20 +0400

    server js added default get
    server js added default get

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 var express = require('express');
-var port = process.env.PORT || 3000;
 var app = express();
-app.listen(port);
+var path    = require("path");
+
+app.use(express.static('app'));
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname+'/app/index.html'));
+});
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+
+}); 
