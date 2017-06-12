c94701b6c42ecf55c3bf44117661140b99bf268d
Rishi <dgreat.rishi@gmail.com>
2017-06-12 10:55:25 +0400

    node app
    node app

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ var express = require('express');
 var app = express();
 var path    = require("path");
 
-app.use(express.static('app'));
-
-
+app.use(express.static('/'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use('/styles',  express.static(__dirname + '/app/styles'));
+app.use('/app',  express.static(__dirname + '/app'));
+app.use('/views',  express.static(__dirname + '/app/views'));
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname+'/app/index.html'));
+  res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 var server = app.listen(3000, function () {
