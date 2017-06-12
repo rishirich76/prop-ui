ffa3053f9d13796314a28de2552b39d907efd3c5
Rishi <dgreat.rishi@gmail.com>
2017-06-12 11:16:23 +0400

    serv
    serv

--- server.js
+++ server.js
@@ -7,15 +7,16 @@ app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 app.use('/styles',  express.static(__dirname + '/app/styles'));
 app.use('/app',  express.static(__dirname + '/app'));
 app.use('/views',  express.static(__dirname + '/app/views'));
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/index.html'));
+});*/
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
-
-var server = app.listen(5000, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
-}); 
